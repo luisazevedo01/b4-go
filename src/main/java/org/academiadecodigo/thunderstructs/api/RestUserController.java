bd07283339235733b3c4486d809b0ffe09495572
Luís Azevedo <luismiguelgomes2001@gmail.com>
2019-12-12 15:49:58 -0100

    implemented crossOrigin

--- src/main/java/org/academiadecodigo/thunderstructs/api/RestUserController.java
+++ src/main/java/org/academiadecodigo/thunderstructs/api/RestUserController.java
@@ -5,13 +5,10 @@ import org.academiadecodigo.thunderstructs.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping(path = "/user")
 public class RestUserController {
@@ -35,6 +32,11 @@ public class RestUserController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity<>(userService.getUserById(username), HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, path = "/register")
+    public ResponseEntity<User> registUser(){
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 }
