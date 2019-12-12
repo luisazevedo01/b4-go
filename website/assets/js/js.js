6d65d862a0c186cac9a0f8592101b1be08e3f7a0
Rafael Azevedo <r95@live.com.pt>
2019-12-12 17:42:57 -0100

    Added Genres filter

--- website/assets/js/js.js
+++ website/assets/js/js.js
@@ -1,19 +1,16 @@
 //SELECT GET CURRENT OPTION
 $('#selectGenre').on('change', function(e) {
     console.log(this.options[e.target.selectedIndex].text);
-    let clubs = $('#clubs');
+    let clubsDiv = $('#clubs');
+    clubsDiv.empty();
 
-    clubs.empty();
-    
-    let str= '<div class="col">isto é um teste</div>';
-
-    $(str).appendTo(clubs);
+    getGenreClubs(this.options[e.target.selectedIndex].text);
 
 });
 
 $(document).ready(function(){
     getGenres();
-    getClubs();
+    getAllClubs();
 });
 
 function getGenres(){
@@ -21,11 +18,9 @@ function getGenres(){
     function successCallback (data) {
         console.log(data);
         let select = $('#selectGenre');
-
         select.empty();
-
+        $('<option selected>Choose a music style</option>').appendTo(select);
         $.each(data,function(index,element){
-            
             let genre = data[index];
             genre = genre.charAt(0).toUpperCase() + genre.slice(1)
             let option = '<option class="option">' + genre + '</option>';
@@ -50,26 +45,26 @@ function getGenres(){
 }
 
 
-function getClubs(){
+function getAllClubs(){
     
     function successCallback (data) {
         console.log(data);
-
         $.each(data,function(index,element){
             let cards = $('#clubs');
-
+            
             let card = '<div class="col">'+
             '<div class="card"> '+
-            '<img src="..." class="card-img-top" alt="..."> '+
+            '<img src="https://i.stack.imgur.com/jZhAM.png" class="card-img-top" alt="..."> '+
             '<div class="card-body">'+
             '<h5 class="card-title">'+ element.name +'</h5>' +
             '<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card\'s content.</p>'+
-              '<a href="#" class="btn btn-primary">Go somewhere</a>'+
+            '<a href="#" class="btn btn-primary">Go somewhere</a>'+
             '</div></div></div>';
-
+            
             $(card).appendTo(cards);
             console.log(element.id);
         });
+        
     }
 
     function errorCallback(){
@@ -83,5 +78,44 @@ function getClubs(){
         success: successCallback,
         error: errorCallback
     });
-    console.log("teste1");
+}
+
+function getGenreClubs(genre){
+    
+    function successCallback (data) {
+        console.log(data);
+        if(data[0] != null){
+
+            $.each(data,function(index,element){
+                let cards = $('#clubs');
+                
+                let card = '<div class="col">'+
+                '<div class="card"> '+
+                '<img src="" class="card-img-top" alt="..."> '+
+                '<div class="card-body">'+
+                '<h5 class="card-title">'+ element.name +'</h5>' +
+                '<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card\'s content.</p>'+
+                '<a href="#" class="btn btn-primary">Go somewhere</a>'+
+                '</div></div></div>';
+                
+                $(card).appendTo(cards);
+                console.log(element.id);
+            });
+        } else {
+            let cards = $('#clubs');
+            $('<div class="col-md-12 text-center mt-5"> No clubs found with this genre </div>').appendTo(cards);
+        }
+    }
+
+    function errorCallback(){
+        alert("There was an error loading clubs");
+    }
+    
+    $.ajax({
+        url: 'http://192.168.1.105:8080/go-go/club/genre/'+genre,
+        async: true,
+        data:{get_param : 'id', get_param : 'name', get_param : 'musicGenre', get_param : 'userList'},
+        success: successCallback,
+        error: errorCallback
+    });
 }
