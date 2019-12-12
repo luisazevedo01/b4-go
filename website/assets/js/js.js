7b0f438360c73b01ee2da076cd7d06b5fc80baf8
Rafael Azevedo <r95@live.com.pt>
2019-12-12 15:40:58 -0100

    added ajax requests

--- website/assets/js/js.js
+++ website/assets/js/js.js
@@ -1,5 +1,5 @@
 //SELECT GET CURRENT OPTION
-$('#select').on('change', function(e) {
+$('#selectGenre').on('change', function(e) {
     console.log(this.options[e.target.selectedIndex].text);
     let clubs = $('#clubs');
 
@@ -10,3 +10,73 @@ $('#select').on('change', function(e) {
     $(str).appendTo(clubs);
 
 });
+
+$(document).ready(function(){
+    getGenres();
+    getClubs();
+});
+
+function getGenres(){
+
+    function successCallback (data) {
+        console.log(data);
+        let select = $('#selectGenre');
+
+        select.empty();
+
+        $.each(data,function(index,element){
+            
+            let genre = data[1];
+
+            console.log(genre);
+        });
+    }
+
+    function errorCallback(){
+        alert("There was an error loading clubs");
+    }
+    
+    $.ajax({
+        url: 'http://192.168.1.105:8080/go-go/genre/',
+        async: true,
+        data:{},
+        success: successCallback,
+        error: errorCallback
+    });
+    
+}
+
+
+function getClubs(){
+    
+    function successCallback (data) {
+        console.log(data);
+
+        $.each(data,function(index,element){
+            let cards = $('#clubs');
+
+            $(cards).after('<div class="card" style="width: 18rem;"> '+
+            '<img src="..." class="card-img-top" alt="..."> '+
+            '<div class="card-body">'+
+            '<h5 class="card-title">'+ element.name +'</h5>' +
+            '<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card\'s content.</p>'+
+              '<a href="#" class="btn btn-primary">Go somewhere</a>'+
+            '</div>'+
+          '</div>');
+            console.log(element.id);
+        });
+    }
+
+    function errorCallback(){
+        alert("There was an error loading clubs");
+    }
+    
+    $.ajax({
+        url: 'http://192.168.1.105:8080/go-go/club/',
+        async: true,
+        data:{get_param : 'id', get_param : 'name', get_param : 'musicGenre', get_param : 'userList'},
+        success: successCallback,
+        error: errorCallback
+    });
+    console.log("teste1");
+}
